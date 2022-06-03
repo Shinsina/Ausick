df961551b8dcfe222955df9fc2696d778d0e2316
Shinsina <shinsina@att.net>
2022-06-03 12:06:38 -0500

    Add the new GraphQL schema files.

---
+++ server/schema/Song.js
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import {
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLObjectType,
+  GraphQLList
+} from 'graphql'
+import fetch from 'node-fetch'
+
+const SongType = new GraphQLObjectType({
+  name: 'Song',
+  fields: () => ({
+    wrapperType: { type: GraphQLString },
+    kind: { type: GraphQLString },
+    artistId: { type: GraphQLInt },
+    collectionId: { type: GraphQLInt },
+    trackId: { type: GraphQLInt },
+    artistName: { type: GraphQLString },
+    collectionName: { type: GraphQLString },
+    trackName: { type: GraphQLString },
+    collectionCensoredName: { type: GraphQLString },
+    trackCensoredName: { type: GraphQLString },
+    artistViewUrl: { type: GraphQLString },
+    collectionViewUrl: { type: GraphQLString },
+    trackViewUrl: { type: GraphQLString },
+    previewUrl: { type: GraphQLString },
+    artworkUrl30: { type: GraphQLString },
+    artworkUrl60: { type: GraphQLString },
+    artworkUrl100: { type: GraphQLString },
+    collectionPrice: { type: GraphQLFloat },
+    trackPrice: { type: GraphQLFloat },
+    releaseDate: { type: GraphQLString },
+    collectionExplicitness: { type: GraphQLString },
+    trackExplicitness: { type: GraphQLString },
+    discCount: { type: GraphQLInt },
+    discNumber: { type: GraphQLInt },
+    trackCount: { type: GraphQLInt },
+    trackNumber: { type: GraphQLInt },
+    trackTimeMillis: { type: GraphQLInt },
+    country: { type: GraphQLString },
+    currency: { type: GraphQLString },
+    primaryGenreName: { type: GraphQLString },
+    isStreamable: { type: GraphQLBoolean }
+  })
+})
+
+const OverallSongDefinitionType = new GraphQLObjectType({
+  name: 'OverallDefinition',
+  fields: () => ({
+    results: { type: new GraphQLList(SongType) },
+    resultCount: { type: GraphQLInt }
+  })
+})
+
+export default {
+  type: OverallSongDefinitionType,
+  args: { url: { type: GraphQLString } },
+  resolve (_, args) {
+    return fetch(args.url).then((data) => data.json())
+  }
+}
