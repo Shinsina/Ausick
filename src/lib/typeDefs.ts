8566017f57a9d314cdfae3cff4a6ab5aaafb2010
Shinsina <shinsina@att.net>
2022-06-06 08:36:39 -0500

    Correct type definitions, include in-use fields.

--- src/lib/typeDefs.ts
+++ src/lib/typeDefs.ts
@@ -11,14 +11,10 @@ export type Artist = {
 export type Album = {
   collectionId: number;
   collectionName: string;
-  collectionCensoredName: string;
   artworkUrl100: string;
   collectionPrice: number;
-  collectionExplicitness: string;
-  trackCount: number;
   copyright: string;
   releaseDate: string;
-  primaryGenreName: string;
   songs: Array<Song>;
 };
 
@@ -26,11 +22,7 @@ export type Song = {
   collectionId: number;
   trackId: number;
   trackName: string;
-  trackCensoredName: string;
   trackPrice: string;
-  trackExplicitness: string;
-  discCount: number;
-  discNumber: number;
   trackNumber: number;
   trackTimeMillis: number;
 };
