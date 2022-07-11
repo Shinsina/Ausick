87dfbd1d7656c70f99fa5f7a90d2fd4f0a71df6e
Shinsina <shinsina@att.net>
2022-07-11 15:10:27 -0500

    "Componentize" home page using ArtistInfo component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,26 +30,14 @@
 </script>
 
 <script lang="ts">
-  import { goto } from '$app/navigation';
+  import type { Artist } from '$lib/typeDefs';
+  import ArtistInfo from '$lib/components/artistInfo.svelte';
 
-  export let artists: Array<Record<string, string>>;
+  export let artists: Array<Artist>;
 </script>
 
 <div class="flex h-screen">
   {#each artists as artist}
-    <div v-for="artist in artistArray" class="flex-col w-1/4 text-center">
-      <div class="flex-auto border-white border-2 border-t-0">
-        <div
-          on:click|preventDefault={() => goto(`/artist/${artist.artistId}`)}
-          class="block lg:text-xl md:text-xs sm:text-xs break-words text-center"
-        >
-          {artist.artistName}
-          <div class="flex justify-center">
-            <img src={artist.photo} alt={artist.artistName} width="380" height="380" />
-          </div>
-          Founded {artist.founded} in {artist.hometown}
-        </div>
-      </div>
-    </div>
+    <ArtistInfo {artist} />
   {/each}
 </div>
