e1fb87cf9eb56fe928616e57197e5c57814ee39a
Shinsina <shinsina@att.net>
2023-03-25 19:38:29 +0000

    Add prefetch to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), prefetch()]
 });
