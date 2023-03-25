b4a3086cb10b4674cfddf9b7c99adf8968ec5955
Shinsina <shinsina@att.net>
2023-03-25 21:00:19 +0000

    Merge homepage endpoint into template

--- src/pages/homepage-data.json.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from 'astro';
-import Artists from '$lib/data/Artist.json';
-
-export const get: APIRoute = async () => {
-  return {
-    body: JSON.stringify(Artists)
-  }
-};
