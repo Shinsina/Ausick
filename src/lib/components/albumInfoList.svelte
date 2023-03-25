e696714a3fea9cba51fbefce2908d1b245b9461d
Shinsina <shinsina@att.net>
2023-03-25 19:26:42 +0000

    Creation of respective list components for Info's

---
+++ src/lib/components/albumInfoList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Artist, Album } from '$lib/typeDefs';
+  import AlbumInfo from '$lib/components/albumInfo.svelte';
+
+  export let artist: Artist;
+
+  const navigateTo = (album: Album) => {
+    return `/artist/${artist.artistId}/album/${album.collectionId}`;
+  };
+</script>
+
+
+<div class="flex flex-wrap w-full">
+  {#each artist.albums as album}
+    <div class="w-1/4 text-center">
+      <AlbumInfo {album} {navigateTo} />
+    </div>
+  {/each}
+</div>
---
+++ src/lib/components/artistInfoList.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Artist } from '$lib/typeDefs';
+  import ArtistInfo from '$lib/components/artistInfo.svelte';
+
+  export let data: Array<Artist>;
+</script>
+
+<div class="flex h-screen">
+  {#each data as artist}
+    <ArtistInfo {artist} />
+  {/each}
+</div>
