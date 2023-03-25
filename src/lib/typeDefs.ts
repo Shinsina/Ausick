24bf35deda57f8fc2ff5a9ed9b7a49134827b3f8
Shinsina <shinsina@att.net>
2023-03-25 19:25:17 +0000

    Migrate and tweak typeDefs.ts

---
+++ src/lib/typeDefs.ts
@@ -0,0 +1,28 @@
+export type Artist = {
+  artistId: number;
+  artistName: string;
+  bio: string;
+  photo: string;
+  founded: number;
+  hometown: string;
+  albums: Array<Album>;
+};
+
+export type Album = {
+  collectionId: number;
+  collectionName: string;
+  artworkUrl100: string;
+  collectionPrice?: number;
+  copyright: string;
+  releaseDate: string;
+  songs: Array<Song>;
+};
+
+export type Song = {
+  collectionId: number;
+  trackId: number;
+  trackName: string;
+  trackPrice?: string;
+  trackNumber: number;
+  trackTimeMillis: number;
+};
