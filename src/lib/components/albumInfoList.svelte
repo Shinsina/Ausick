1a1a745aa3a0d4920347ad2087b6ad7e5ff4a81b
Shinsina <shinsina@att.net>
2023-03-26 00:03:49 +0000

    Replace navigateTo w/dynamic string, alter styles

--- src/lib/components/albumInfoList.svelte
+++ src/lib/components/albumInfoList.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
-  import type { Artist, Album } from '$lib/typeDefs';
+  import type { Artist } from '$lib/typeDefs';
   import AlbumInfo from '$lib/components/albumInfo.svelte';
 
   export let artist: Artist;
-
-  const navigateTo = (album: Album) => {
-    return `/Ausick/artist/${artist.artistId}/album/${album.collectionId}`;
-  };
 </script>
 
 
 <div class="flex flex-wrap w-full">
   {#each artist.albums as album}
-    <div class="w-1/4 text-center">
-      <AlbumInfo {album} {navigateTo} />
+    <div class="w-full md:w-1/4 text-center">
+      <AlbumInfo
+        {album}
+        navigateTo={`/Ausick/artist/${artist.artistId}/album/${album.collectionId}`}
+      />
     </div>
   {/each}
 </div>
